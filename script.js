702a1ecda6532cb2befe298badad7c9ccb5eb76f
Michael-hm <michael.hm2000@gmail.com>
2026-02-03 01:48:48 +0100

    confianza

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ async function sendToBackend(sequence) {
     const data = await res.json();
     predictionEl.textContent = data.label;
     confidenceEl.textContent =
-      `Confianza: ${(data.confidence * 100).toFixed(1)}%`;
+      `Confianza: ${(data.confidence * 1000).toFixed(1)}%`;
   } catch (e) {
     console.error(e);
   }
